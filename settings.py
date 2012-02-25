c23215135cb0db145a3a2fede483d07b5b9606ce
Jiachen Yang <farseerfc@gmail.com>
2012-02-25 19:55:10 +0900

    changed settings again

--- settings.py
+++ settings.py
@@ -4,16 +4,16 @@
 TIMEZONE = 'Asia/Tokyo'
 DATE_FORMATS = {
     'en':'%a, %d %b %Y',
-    'zh':'%Y年%m月%d日, 周%a',
-    'jp':'%Y年%ｍ月%d日(%a)',
+    'zh':'%Y-%m-%d, %a',
+    'jp':'%Y/%m/%d (%a)',
 }
 # windows locale: http://msdn.microsoft.com/en-us/library/cdax410z%28VS.71%29.aspx
-LOCALE = ['chn', 'jpn','usa',       # windows
-        'en_US', 'zh_CN', 'ja_JP']  # Unix/Linux
-DEFAULT_LANG = 'zh'
+LOCALE = ['usa', 'chn', 'jpn',        # windows
+          'en_US', 'zh_CN', 'ja_JP']  # Unix/Linux
+DEFAULT_LANG = 'en'
 
 SITENAME = 'Farseerfc Blog'
-AUTHOR = 'Jiachen Yang (farseerfc@gmail.com)'
+AUTHOR = 'Jiachen Yang'
 
 DISQUS_SITENAME = 'farseerfcgithub'
 GITHUB_URL = 'https://github.com/farseerfc'
@@ -24,7 +24,10 @@ SOCIAL = (('twitter', 'http://twitter.com/farseerfc'),
           ('github', 'https://github.com/farseerfc'),
           ('facebook', 'http://www.facebook.com/farseerfc'),
           ('weibo', 'http://weibo.com/farseerfc'),
+          ('renren', 'http://www.renren.com/farseer'),
           )
+          
+
 TWITTER_USERNAME = 'farseerfc'
 
 THEME='notmyidea'
