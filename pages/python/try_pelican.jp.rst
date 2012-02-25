383be22e1810e5a503df031dac86696181951b60
Jiachen Yang <farseerfc@gmail.com>
2012-02-25 18:25:42 +0900

    try japanese

---
+++ pages/python/try_pelican.jp.rst
@@ -0,0 +1,33 @@
+Pelicanを試してみた
+=========================
+
+:slug: try-pelican
+:lang: jp
+
+Pelicanで書いたの初めてのblog，多言語の機能を試しています。
+
+我的设置 settings.py
+++++++++++++++++++++++++
+
+.. code-block:: python
+    
+    # -*- coding: utf-8 -*-
+    ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/'
+    DATE_FORMATS={'en':'%d/%m/%Y','zh':'%Y-%m-%d','jp':'%Y-%m-%d'}
+    AUTHOR = 'Jiachen Yang'
+    DISQUS_SITENAME = 'farseerfcgithub'
+    GITHUB_URL = 'https://github.com/farseerfc'
+    SITEURL = 'http://farseerfc.github.com'
+    SITENAME = 'Farseerfc Blog'
+    SOCIAL = (('twitter', 'http://twitter.com/farseerfc'),
+              ('github', 'https://github.com/farseerfc'),
+              ('facebook', 'http://www.facebook.com/farseerfc'),)
+    TAG_FEED = 'feeds/%s.atom.xml'
+    THEME='notmyidea'
+    TWITTER_USERNAME = 'farseerfc'
+    LOCALE = 'en'
+    DEFAULT_CATEGORY ='Others'
+    OUTPUT_PATH = '.'
+    PATH = 'pages'
+    TIMEZONE = 'Asia/Tokyo'
+
--- settings.py
+++ settings.py
@@ -1,19 +1,39 @@
 
 # -*- coding: utf-8 -*-
-ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/'
-DATE_FORMATS={'en':'%d/%m/%Y','zh':'%Y-%m-%d','jp':'%Y-%m-%d'}
+
+DATE_FORMATS = {
+    'en':'%a, %d %b %Y',
+    'zh':'%Y %b %d, %a',
+    'jp':'%Y %b %d(%a)',
+}
+
+## locale setting is only valid on windows, see 
+## http://msdn.microsoft.com/en-us/library/cdax410z%28VS.71%29.aspx
+LOCALE = ['usa', 'chn', 'jpn'] 
+## On Unix/Linux, use:
+# LOCALE = ['en_US', 'zh_CN', 'ja_JP'] 
+
+TAG_FEED  = 'feeds/%s.atom.xml'
+
+SITENAME = 'Farseerfc Blog'
 AUTHOR = 'Jiachen Yang'
+
 DISQUS_SITENAME = 'farseerfcgithub'
 GITHUB_URL = 'https://github.com/farseerfc'
 SITEURL = 'http://farseerfc.github.com'
-SITENAME = 'Farseerfc Blog'
+
 SOCIAL = (('twitter', 'http://twitter.com/farseerfc'),
           ('github', 'https://github.com/farseerfc'),
-          ('facebook', 'http://www.facebook.com/farseerfc'),)
-TAG_FEED = 'feeds/%s.atom.xml'
-THEME='notmyidea'
+          ('facebook', 'http://www.facebook.com/farseerfc'),
+          ('weibo', 'http://weibo.com/farseerfc'),
+          )
 TWITTER_USERNAME = 'farseerfc'
-LOCALE = 'en'
+
+
+THEME='notmyidea'
+
+
+
 DEFAULT_CATEGORY ='Others'
 OUTPUT_PATH = '.'
 PATH = 'pages'
