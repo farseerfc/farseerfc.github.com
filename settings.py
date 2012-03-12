aec777c1bb31f2592f2a163898fafaf1f4828ad1
Jiachen Yang <farseerfc@gmail.com>
2012-03-12 20:23:30 +0900

    add google custom search

--- settings.py
+++ settings.py
@@ -36,6 +36,19 @@ SIDEBAR_CUSTOM = r"""
 <iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" 
 src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=1&ptype=1&speed=0&skin=2&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1862842353&verifier=b193b9de&dpc=1">
 </iframe>
+<li class="nav-header"><h4><i class="icon-search"></i>Google Search</h4></li>
+<div id="cse" style="width: 100%;">Loading</div>
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<script type="text/javascript"> 
+  google.load('search', '1', {language : 'zh-CN'});
+  google.setOnLoadCallback(function() {
+    var customSearchOptions = {};  var customSearchControl = new google.search.CustomSearchControl(
+      '001578481551708017171:axpo6yvtdyg', customSearchOptions);
+    customSearchControl.setResultSetSize(google.search.Search.SMALL_RESULTSET);
+    customSearchControl.draw('cse');
+  }, true);
+</script>
+<link rel="stylesheet" href="http://www.google.com/cse/style/look/default.css" type="text/css" />
 """
 
 THEME='bootstrap2'
