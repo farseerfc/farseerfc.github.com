a4df3e4a7ed6b065131175cf9bf98261a9b920fe
Jiachen Yang <farseerfc@gmail.com>
2013-03-02 03:29:20 +0900

    change to dark theme

--- settings.py
+++ settings.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
-import sys
 
 TIMEZONE = 'Asia/Tokyo'
 
 DATE_FORMATS = {
-    'en' :( (u'en_US','utf8'),#'usa',
-	    u'%a, %d %b %Y',),
-    'zh' :( (u'zh_HK','utf8'),#'cht',
-	    u'%Y-%m-%d',),
-    'zhs':( (u'zh_CN','utf8'),#'chs',
-	    u'%Y-%m-%d',),
-    'jp' :( (u'ja_JP','utf8'),#'jpn',
-	    u'%Y/%m/%d(%a)',),
+    'en': ((u'en_US', 'utf8'), u'%a, %d %b %Y',),
+    'zh': ((u'zh_HK', 'utf8'), u'%Y年%m月%d日(週%a)',),
+    'zhs': ((u'zh_CN', 'utf8'), u'%Y年%m月%d日(周%a)',),
+    'jp': ((u'ja_JP', 'utf8'), u'%Y年%m月%d日(%a)',),
 }
 # windows locale: http://msdn.microsoft.com/en-us/library/cdax410z%28VS.71%29.aspx
 #LOCALE = [#'usa', 'cht', 'chs', 'jpn',        # windows
@@ -25,7 +20,7 @@ DISQUS_SITENAME = 'farseerfcgithub'
 GITHUB_URL = 'https://github.com/farseerfc'
 SITEURL = 'http://farseerfc.github.com'
 GOOGLE_ANALYTICS = 'UA-29540705-1'
-TAG_FEED_ATOM  = 'feeds/%s.atom.xml'
+TAG_FEED_ATOM = 'feeds/%s.atom.xml'
 
 SOCIAL = (('twitter', 'http://twitter.com/farseerfc'),
           ('github', 'https://github.com/farseerfc'),
@@ -33,26 +28,22 @@ SOCIAL = (('twitter', 'http://twitter.com/farseerfc'),
           ('weibo', 'http://weibo.com/farseerfc'),
           ('renren', 'http://www.renren.com/farseer'),
           )
-          
 
 TWITTER_USERNAME = 'farseerfc'
 SIDEBAR_CUSTOM = r"""
 <li class="nav-header"><h4><i class="icon-list-alt"></i>Weibo</h4></li>
-<iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" 
-src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=1&ptype=1&speed=0&skin=2&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1862842353&verifier=b193b9de&dpc=1">
-</iframe>
+<iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=zh_tw&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=1&noborder=0&isWeibo=1&isFans=1&uid=1862842353&verifier=b193b9de&colors=33b5e5,121417,999999,ade1f4,ffffff&dpc=1"></iframe>
 """
 
 GOOGLE_CUSTOM_SEARCH_SIDEBAR = "001578481551708017171:axpo6yvtdyg"
 GOOGLE_CUSTOM_SEARCH_NAVBAR = "001578481551708017171:hxkva69brmg"
 
-THEME='bootstrap2'
+THEME = 'bootstrap2'
 #THEME='notmyidea'
-CSS_FILE = "wide.css"
+#CSS_FILE = "wide.css"
 
-DEFAULT_CATEGORY ='Others'
+DEFAULT_CATEGORY = 'Others'
 OUTPUT_PATH = '.'
 PATH = 'posts'
 
 #PDF_GENERATOR = True
-
